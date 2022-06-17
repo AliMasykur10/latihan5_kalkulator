826260c9c3f1963ab26774d4b0f3234a61229df7
AliMasykur10 <alimasykur2001@gmail.com>
2022-06-18 06:22:56 +0700

    menambahkan tombol dark mode

--- scrpit.js
+++ scrpit.js
@@ -21,3 +21,18 @@ function bersihkan() {
 function del() {
   output.value = output.value.slice(0, -1);
 }
+
+// dark mode
+
+let body = document.querySelector(".body");
+modeMalam = document.querySelector(".mode-malam");
+
+function darkmode() {
+  body.classList.add("mode-malam");
+}
+
+function modeMalam() {
+  if ((modeMalam = true)) {
+    body.classList.remove("mode-malam");
+  }
+}
