f81987a4e80928559ea33b3dbdfca4b7c6004fee
AliMasykur10 <alimasykur2001@gmail.com>
2022-06-19 06:38:01 +0700

    menyelesaikan fitur dark mode

--- scrpit.js
+++ scrpit.js
@@ -16,23 +16,12 @@ function bersihkan() {
   output.value = "";
 }
 
-// console.log(clear);
-
 function del() {
   output.value = output.value.slice(0, -1);
 }
 
 // dark mode
 
-let body = document.querySelector(".body");
-modeMalam = document.querySelector(".mode-malam");
-
 function darkmode() {
-  body.classList.add("mode-malam");
-}
-
-function modeMalam() {
-  if ((modeMalam = true)) {
-    body.classList.remove("mode-malam");
-  }
+  document.body.classList.toggle("dark-mode");
 }
