e0b1ea4fd00ce68d90b2738e1160f10f1ff1eb81
AliMasykur10 <alimasykur2001@gmail.com>
2022-06-21 07:00:13 +0700

    memnambah fungsi kalkutaor dan sedikit style

---
+++ pematangan/script.js
@@ -0,0 +1,21 @@
+let keluaran = document.getElementById("hasil");
+
+function display(num) {
+  keluaran.value += num;
+}
+
+function hasil() {
+  try {
+    keluaran.value = eval(keluaran.value);
+  } catch (err) {
+    alert("invalid");
+  }
+}
+
+function del() {
+  keluaran.value = keluaran.value.slice(0, -1);
+}
+
+function bersihkan() {
+  keluaran.value = "";
+}
